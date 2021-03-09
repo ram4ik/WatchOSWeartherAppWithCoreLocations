0f46810652659cbededca28fd445bb636047e833
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-03-09 21:14:11 +0200

    Initial Commit

---
+++ WatchOSWeartherAppWithCoreLocations/WatchOSWeartherAppWithCoreLocationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchOSWeartherAppWithCoreLocationsApp.swift
+//  WatchOSWeartherAppWithCoreLocations
+//
+//  Created by ramil on 09.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct WatchOSWeartherAppWithCoreLocationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
